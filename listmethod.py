5dce09eee2e563f404fd74365ac5c148ab9c0a7e
ThajunnisaTK <thajunnisazameen@gmail.com>
2023-04-26 13:52:07 +0530

    add listmethod

---
+++ listmethod.py
@@ -0,0 +1,8 @@
+# lst = [4,5,6]
+# lst2 = [9,8]
+# lst.append(lst2)
+# print(lst)  #nested list #in order to  avoid this we chose extend function
+lst = [4,5,6]
+lst3 = [1,2,3,4,5,6]
+lst3.extend(lst)
+print(lst3)
