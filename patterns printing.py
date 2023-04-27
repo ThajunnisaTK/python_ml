2db45043dac8d392fb6ae8c37deef18c0cc94bd8
ThajunnisaTK <thajunnisazameen@gmail.com>
2023-04-27 07:53:16 +0530

    add patterns

--- patterns printing.py
+++ patterns printing.py
@@ -1,45 +1,52 @@
-# for i in range(1,5):
-#      for j in range(1,4):
-#          print(1,end=" ")
-#      print()
-#
-#  for i in range(1,5):
-#      for j in range(1,4):
-#          print("*",end=" ")
-#      print()
-#
-#  for i in range(1,7):
-#      for j in range(1,7):
-#          print(",end=" ")
-#      print()
-# for i in range(1, 5):
-#     for j in range(1, 4):
-#         print(j + 4, end=" ")
-#     print()
-# for i in range(1,5):
-#     for j in range(i):
-#         print(i, end=" ")
-#     print()
-# for i in range(1,5):
-#     for j in range(1,i+1):
-#         print(i, end=" ")
-#     print()
-# for i in range(1,5):
-#     for j in range(i):
-#         print(j+1,end="")
-#     print()
-# for i in range(1,5):
-#     for j in range(i,5):
-#         print(i, end="")
-#     print()
-# for i in range(1,5):
-#     for j in range(5-i):
-#         print(i, end="")
-#     print()
-# for i in range(1,7):
-#     for j in range(1,7):
-#         print("*", end="")
-#     print()
+for i in range(1,5):
+     for j in range(1,4):
+         print(1,end=" ")
+     print()
+
+ for i in range(1,5):
+     for j in range(1,4):
+         print("*",end=" ")
+     print()
+
+ for i in range(1,7):
+     for j in range(1,7):
+         print(",end=" ")
+     print()
+
+for i in range(1, 5):
+    for j in range(1, 4):
+        print(j + 4, end=" ")
+    print()
+
+for i in range(1,5):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(i, end=" ")
+    print()
+
+for i in range(1,5):
+    for j in range(i):
+        print(j+1,end="")
+    print()
+
+for i in range(1,5):
+    for j in range(i,5):
+        print(i, end="")
+    print()
+
+for i in range(1,5):
+    for j in range(5-i):
+        print(i, end="")
+    print()
+for i in range(1,7):
+    for j in range(1,7):
+        print("*", end="")
+    print()
+
 for i in range(1, 7):
     for j in range(1, 7):
         if (j == 1 or i == 6 or i == j):
---
+++ supermarket.py
@@ -0,0 +1,5 @@
+items = ["mango","chips","icecream","banana","cucumber","pen","juice","candy","bag","bucket"]
+price = [25,50,15,55,18,10,45,5,400,130]
+item_bought = input("enter items")
+item_bought
+number_of_items = len(item_bought)
